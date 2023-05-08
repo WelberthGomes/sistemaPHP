bc5c244702f997323544944a858e240f8ef4b5ad
Welberth Gomes <Welberth.wel@gmail.com>
2023-05-08 13:55:27 -0300

    Organizando projeto

--- index.php
+++ index.php
@@ -1,12 +1,8 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 <?php
-require_once 'configuracao.php';
-require_once 'util\Util.php';
-require_once 'shared\Mensagem.php';
-require_once 'shared\Controlador.php';
+require 'vendor/autoload.php';
 
-use sistema\util\Util;
 use sistema\shared\Controlador;
-
-echo Util::fomataNumero(10000);
 new Controlador;
+
+echo NOME_DO_SISTEMA;
